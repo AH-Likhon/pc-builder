6bf6d666b2b2cec2262fdbcb2ff61a9b2456e8d5
AH-Likhon <ictianlikhon6@gmail.com>
2023-09-21 12:31:51 +0600

    [API] Successfuly connected api, get and shown featured products

--- src/components/UI/FeaturedCategories.js
+++ src/components/UI/FeaturedCategories.js
@@ -12,7 +12,9 @@ import Link from "next/link";
 const FeaturedCategories = () => {
   return (
     <div className="w-11/12 mx-auto pb-10">
-      <h1 className="text-5xl text-center my-14">Featured Categories</h1>
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-center my-8 md:my-10 lg:my-14">
+        Featured Categories
+      </h1>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 md:gap-8">
         <div className="w-11/12 md:w-[200px] h-52 flex items-center justify-center flex-col shadow-2xl rounded-lg py-0 px-0">
           <Link href="/ram">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import RootLayout from "@/components/Layout/RootLayout";
 import Banner from "@/components/UI/Banner";
 import FeaturedCategories from "@/components/UI/FeaturedCategories";
+import FeaturedProducts from "@/components/UI/FeaturedProducts";
 
-const Home = () => {
+const Home = ({ data }) => {
   return (
     <>
       <Banner />
       <FeaturedCategories />
+      <FeaturedProducts data={data?.data} />
     </>
   );
 };
@@ -15,4 +17,18 @@ export default Home;
 
 Home.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  if (typeof window === "undefined") {
+    const res = await fetch("http://localhost:3000/api/products");
+    console.log(res);
+    const data = await res.json();
+    return {
+      props: {
+        data,
+      },
+      revalidate: 10,
+    };
+  }
 };
